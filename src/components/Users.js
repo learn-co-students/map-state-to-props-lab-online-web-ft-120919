d95175fbecab62add19801100623e16309c26b75
github username <meliajm@gmail.com>
2020-04-27 16:58:25 -0700

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,18 +5,23 @@ import { connect } from 'react-redux'
 // add any needed imports here
 class Users extends Component {
 
+  
   render() {
+  const userElem = this.props.users.map( user => <li>{user.username}</li>)
+    // console.log('user elem', userElem)
     return (
       <div>
         <ul>
           Users!
           
-          <li>{this.props.users.map( user => user)}</li>
+
+          {userElem}
+          
           <br></br>
           <br></br>
 
-          User count:
-          {/* {mapStateToProps.users.length} */}
+          User count: <br></br>
+          {userElem.length}
         </ul>
       </div>
     )
